e9f01394c75e66a863ea03e3b15cd062da59248d
Gerry Manoim <gmanoim@quantopian.com>
2020-06-26 03:37:23 -0400

    BUG: no imports outside modules

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-import { getInput, info } from '@actions/core';
-import { getOctokit, context } from '@actions/github';
+const core = require('@actions/core');
+const github = require('@actions/github');
 
 async function run() {
-    const token = process.env.GITHUB_TOKEN
-    const octokit = getOctokit(token)
+    const token = core.getInput('repo-token')
+    const octokit = github.getOctokit(token)
   
-    if (!context.payload.pull_request) {
+    if (!github.context.payload.pull_request) {
     throw new Error(
         "Payload doesn't contain `pull_request`. Make sure this Action is being triggered by a pull_request event (https://help.github.com/en/articles/events-that-trigger-workflows#pull-request-event-pull_request)."
     )
     }
-    const title = context.payload.pull_request.title
-    info(title)
+    const title = github.context.payload.pull_request.title
+    core.info(title)
   
     let prefix_map = {
         "API": "api",
@@ -39,9 +39,9 @@ async function run() {
 
     if (label !== null) {
         await octokit.issues.addLabels({
-            owner: context.repo.owner,
-            repo: context.repo.repo,
-            issue_number: context.payload.pull_request.number,
+            owner: github.context.repo.owner,
+            repo: github.context.repo.repo,
+            issue_number: github.context.payload.pull_request.number,
             labels: label
           })
     }
