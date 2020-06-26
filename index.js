29e23f6f0732a2e6fd3b1cbdfcc740f21b1e49a7
Gerry Manoim <gmanoim@quantopian.com>
2020-06-26 03:40:52 -0400

    BUG: token from env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 async function run() {
-    const token = core.getInput('repo-token')
+    const token = process.env.GITHUB_TOKEN
     const octokit = github.getOctokit(token)
   
     if (!github.context.payload.pull_request) {
